816fa81cc7c4b210d38d01190332370d420aa771
coojac09 <49573587+coojac09@users.noreply.github.com>
2019-07-29 12:28:38 -0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIRS=()
+
+cat /tmp/edurange-roguelike/motd/message.txt >> /etc/motd
+
+for FILE in /home/*; do
+	if [[ -d $FILE ]]; then 
+		DIRS+=( "$FILE" )
+		echo $FILE
+	fi
+done
+
+cp /bin/bash /bin/rbash
+
+#for studentDIR in "${DIRS[@]}"; do
+	#player=$(basename $studentDIR)
+	#cp /tmp/edurange-roguelike/motd/motd/message.txt /$studentDIR/
+
+	#usermod -s /bin/rbash $player
+	#mkdir $studentDIR/Entrance
+
